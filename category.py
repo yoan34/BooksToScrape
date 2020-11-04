69e830c31e7801ac6e613e908360f30dc4539bdc
Bousquet <bousquet_yoan11@yahoo.fr>
2020-11-04 15:15:20 +0100

    Récupère tous les livres de toutes les pages d'une catégorie.

--- category.py
+++ category.py
@@ -11,35 +11,54 @@ CSV_HEADERS = ['product_page_url', 'universal_product_code', 'title', 'price_inc
     'price_excluding_tax', 'number_available', 'product_description', 'category',
     'review_rating', 'image_url']
 
+
 def get_books_from_category(url):
-    response = requests.get(url)
+    response, pages = requests.get(url), []
 
     if response.ok:
-        category = url[51:].split('_')[0]
+        category = url[51:].split('_')[0] # Récupère le nom de la catagory
         filename = 'books_' + category + '.csv'
 
         with open(filename, 'w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file) # Création d'un object ou l'on peut écrire/convertir des données en csv.
             writer.writerow(CSV_HEADERS) # On ajoute les en-têtes en première ligne du fichier csv.
 
+            #Récupère le bloc HTML de la page web.
             soup = BeautifulSoup(response.text, 'html.parser')
-            # récupère le bloc HTML de chaque livre
-            books = soup.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
-                
+            number_of_pages = soup.find('li', {'class': 'current'})
 
-            # Pour chaque livre, on isole son lien et on l'ajoute a une URL de base.
-            for book in books:
-                link = 'http://books.toscrape.com/catalogue' +  book.find('a')['href'][8:]
-                data = get_book(link)
+            # Cette condition permet d'ajouter les urls des différentes pages si elles existent, sinon
+            # on ajoute simplement l'url de base.
+            if number_of_pages:
+                number_of_pages = int(number_of_pages.text.split()[-1]) 
+                for page in range(number_of_pages):
+                    pages.append('/'.join(url.split('/')[:-1]) + '/page-{}.html'.format(page+1))
+            else:
+                pages.append(url)
+
                     
-                # Utilise la solution d'encodage et décodage pour afficher correctement £ et autres dans 
-                # le fichier CSV (seul solution trouvé).
-                writer.writerow([d.encode('raw_unicode_escape').decode('utf-8') for d in data])
+            # On navigue à travers toutes les urls disponibles dans la liste 'pages'.
+            for page in pages:
+                response = requests.get(page)
+
+                if response.ok:
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    books = soup.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+
+                    # on récupère le lien de chaque livre et leurs informations souhaitées.
+                    for book in books:
+                        link = 'http://books.toscrape.com/catalogue' +  book.find('a')['href'][8:]
+                        data = get_book(link)
+                                    
+                        # Utilise la solution d'encodage et décodage pour afficher correctement £ et autres
+                        # caractères dans le fichier CSV (seul solution trouvé).
+                        writer.writerow([d.encode('raw_unicode_escape').decode('utf-8') for d in data])
+
     else:
         print('Error loading page.')
 
 
 # Tester le script sans l'importer
 if __name__ == '__main__':
-    get_books_from_category('http://books.toscrape.com/catalogue/category/books/nonfiction_13/index.html')
+    get_books_from_category('http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html')
 
